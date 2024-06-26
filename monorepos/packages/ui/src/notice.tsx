39628d8592ee124f22f155ef5fc443c88e2104aa
Aashim Limbu <thelastmanarmy37@gmail.com>
2024-06-26 14:14:00 +0545

    initializing the turbo repo

---
+++ monorepos/packages/ui/src/notice.tsx
@@ -0,0 +1,24 @@
+export function Notice(): JSX.Element {
+	return (
+		<div
+			style={{
+				width: "100%",
+				display: "flex",
+				justifyContent: "center",
+				alignContent: "center",
+			}}
+		>
+			<div
+				style={{
+					backgroundColor: "green",
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center",
+
+				}}
+			>
+				<h1>Hello From Aashim</h1>
+			</div>
+		</div>
+	);
+}
